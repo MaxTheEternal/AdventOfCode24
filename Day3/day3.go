02198cbb1935443ae2ae9dc9bd9f98791c5dec5f
max <max.heinrich.de@gmail.com>
2024-12-03 14:42:32 +0100

    finished day 3 part 2

--- Day3/day3.go
+++ Day3/day3.go
@@ -12,48 +12,105 @@ import (
 
 func main() {
 	file := "./Day3/day3_input.txt"
-	sum := correctMuls(file)
-	fmt.Printf("\nSum is: %v\n", sum)
+	fmt.Printf("Part 1: %v\n", correctMuls(file))
+	fmt.Printf("Part 2: %v\n", correctMulsWithInstructions(file))
+}
+
+func correctMulsWithInstructions(file string) int {
+	lines := readFile(file)
+	sum := 0
+	muls := getMulsWithInstructions(lines)
+	for _, v := range muls {
+		sum += calcProductFromMul(v)
+	}
+	return sum
 }
 
 func correctMuls(file string) int {
+	lines := readFile(file)
+	sum := 0
+	muls := getAllMuls(lines)
+	for _, v := range muls {
+		sum += calcProductFromMul(v)
+	}
+	return sum
+}
+
+func readFile(file string) []string {
 	openFile, err := os.Open(file)
 	if err != nil {
 		log.Fatal(err)
 	}
-	sum := 0
 
+	lines := []string{}
 	scanner := bufio.NewScanner(openFile)
 	for scanner.Scan() {
 		line := scanner.Text()
 		if line == "" {
 			continue
 		}
-		r, _ := regexp.Compile("mul\\([0-9]{1,3},[0-9]{1,3}\\)")
+		lines = append(lines, line)
+	}
+	return lines
 
+}
+func getMulsWithInstructions(lines []string) []string {
+	muls := []string{}
+	const doString string = "do()"
+	const dontString string = "don't()"
+
+	enabled := true
+
+	// /(mul\([0-9]{1,3},[0-9]{1,3}\)|do\(\)|don't\(\))/gm
+	r, err := regexp.Compile(`(mul\([0-9]{1,3},[0-9]{1,3}\)|do\(\)|don't\(\))`)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, line := range lines {
 		matches := r.FindAllString(line, -1)
-
 		for _, v := range matches {
-
-			v = strings.TrimPrefix(v, "mul(")
-			v = strings.TrimSuffix(v, ")")
-
-			nums := strings.Split(v, ",")
-			if len(nums) != 2 {
-				log.Fatal("Mold didnt contain 2 values")
+			if v == doString {
+				enabled = true
+			} else if v == dontString {
+				enabled = false
+			} else if enabled {
+				muls = append(muls, v)
 			}
 
-			number1, err := strconv.Atoi(nums[0])
-			if err != nil {
-				log.Fatal(err)
-
-			}
-			number2, err := strconv.Atoi(nums[1])
-			if err != nil {
-				log.Fatal(err)
-			}
-			sum += number1 * number2
 		}
 	}
-	return sum
+	return muls
+}
+func getAllMuls(lines []string) []string {
+	muls := []string{}
+
+	r, _ := regexp.Compile(`mul\([0-9]{1,3},[0-9]{1,3}\)`)
+
+	for _, line := range lines {
+		matches := r.FindAllString(line, -1)
+		muls = append(muls, matches...)
+	}
+	return muls
+}
+
+func calcProductFromMul(mul string) int {
+	mul = strings.TrimPrefix(mul, "mul(")
+	mul = strings.TrimSuffix(mul, ")")
+
+	nums := strings.Split(mul, ",")
+	if len(nums) != 2 {
+		log.Fatal("Mol didnt contain 2 values")
+	}
+
+	number1, err := strconv.Atoi(nums[0])
+	if err != nil {
+		log.Fatal(err)
+
+	}
+	number2, err := strconv.Atoi(nums[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+	return number1 * number2
 }
--- Day3/day3_test.go
+++ Day3/day3_test.go
@@ -1,6 +1,8 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
 func Test_correctMuls(t *testing.T) {
 	type args struct {
@@ -21,6 +23,30 @@ func Test_correctMuls(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := correctMuls(tt.args.file); got != tt.want {
 				t.Errorf("correctMuls() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_correctMulsWithInstructions(t *testing.T) {
+	type args struct {
+		file string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			"part 2 test",
+			args{"./day_3_input_test_part2.txt"},
+			48,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := correctMulsWithInstructions(tt.args.file); got != tt.want {
+				t.Errorf("correctMulsWithInstructions() = %v, want %v", got, tt.want)
 			}
 		})
 	}
