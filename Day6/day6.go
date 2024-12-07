3760ae19200b1517e564a4e0ca2301071164cd1c
MaxTheEternal <98349129+MaxTheEternal@users.noreply.github.com>
2024-12-07 11:59:01 +0100

    Finished Day 6 Part 2 with concurrency

--- Day6/day6.go
+++ Day6/day6.go
@@ -7,19 +7,41 @@ import (
 	"os"
 	"slices"
 	"strings"
+	"sync"
+	"time"
 )
+
+type recordedPosition struct {
+	X    int
+	Y    int
+	XDir int
+	YDir int
+}
+type safeCounter struct {
+	m       sync.Mutex
+	counter int
+}
+
+func (c *safeCounter) Inc() {
+	c.m.Lock()
+	c.counter++
+	c.m.Unlock()
+}
 
 func Day6() {
 	file := "./Day6/day6_input.txt"
 	fmt.Println("Day 6")
 	fmt.Printf("Part 1: %v\n", partOne(file))
+	start := time.Now()
+	fmt.Printf("Part 2: %v\n", PartTwo(file))
+	elapsed := time.Since(start)
+	fmt.Printf("It took: %v\n", elapsed)
 
 }
 
 func partOne(path string) int {
 	matrix, x, y := readFile(path)
 	running := true
-
 	xDir := 0
 	yDir := -1
 
@@ -80,6 +102,91 @@ func partOne(path string) int {
 		}
 	}
 	return numberOfX
+}
+
+func PartTwo(path string) int {
+	matrix, guardX, guardY := readFile(path)
+	var wg sync.WaitGroup
+	coutner := safeCounter{sync.Mutex{}, 0}
+
+	for yIndex, line := range matrix {
+		for xIndex, char := range line {
+			if char == "#" {
+				continue
+			}
+			if xIndex == guardX && yIndex == guardY {
+				continue
+			}
+			wg.Add(1)
+			go func() {
+				defer wg.Done()
+				if isLooping(matrix, guardX, guardY, xIndex, yIndex) {
+					coutner.Inc()
+				}
+			}()
+		}
+	}
+
+	wg.Wait()
+
+	return coutner.counter
+}
+
+func isLooping(matrix [][]string, x, y, xBlock, yBlock int) bool {
+	positions := []recordedPosition{}
+	running := true
+
+	xDir := 0
+	yDir := -1
+
+	yLen := len(matrix)
+	if yLen == 0 {
+		log.Fatal("Matirx shouldnt be empty")
+	}
+	xlen := len(matrix[0])
+
+	for running {
+		nextX := x + xDir
+		nextY := y + yDir
+
+		if nextX == xlen || nextX < 0 {
+			return false
+		}
+		if nextY == yLen || nextY < 0 {
+			return false
+		}
+
+		// fmt.Printf("X: %v Y: %v\n", x, y)
+		currentPosition := recordedPosition{x, y, xDir, yDir}
+		if slices.Contains(positions, currentPosition) {
+			return true
+		}
+		positions = append(positions, currentPosition)
+
+		virtualBlocker := nextX == xBlock && nextY == yBlock
+		blockerIsNext := matrix[nextY][nextX] == "#"
+		if blockerIsNext || virtualBlocker {
+			// Turning 90 Degrees
+			switch {
+			case xDir == 0 && yDir == -1:
+				xDir = 1
+				yDir = 0
+			case xDir == 1 && yDir == 0:
+				xDir = 0
+				yDir = 1
+			case xDir == 0 && yDir == 1:
+				xDir = -1
+				yDir = 0
+			case xDir == -1 && yDir == 0:
+				xDir = 0
+				yDir = -1
+			}
+		} else {
+			x = nextX
+			y = nextY
+		}
+	}
+	return true
 }
 
 func readFile(path string) ([][]string, int, int) {
--- Day6/day6_test.go
+++ Day6/day6_test.go
@@ -1,6 +1,8 @@
 package day6
 
-import "testing"
+import (
+	"testing"
+)
 
 func Test_partOne(t *testing.T) {
 	type args struct {
@@ -23,6 +25,92 @@ func Test_partOne(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := partOne(tt.args.path); got != tt.want {
 				t.Errorf("partOne() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_isLooping(t *testing.T) {
+	type args struct {
+		matrix   [][]string
+		x        int
+		y        int
+		xBlocker int
+		yBlocker int
+	}
+	matrix, x, y := readFile("./day6_input_test.txt")
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			"test looping 1",
+			args{matrix, x, y, 3, 6},
+			true,
+		},
+		{
+			"test looping 2",
+			args{matrix, x, y, 6, 7},
+			true,
+		},
+		{
+			"test looping 3",
+			args{matrix, x, y, 7, 7},
+			true,
+		},
+		{
+			"test looping 4",
+			args{matrix, x, y, 1, 8},
+			true,
+		},
+		{
+			"test looping 5",
+			args{matrix, x, y, 3, 8},
+			true,
+		},
+		{
+			"test looping 6",
+			args{matrix, x, y, 7, 9},
+			true,
+		},
+		{
+			"test not looping",
+			args{matrix, x, y, -1, -1},
+			false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := isLooping(tt.args.matrix, tt.args.x, tt.args.y, tt.args.xBlocker, tt.args.yBlocker); got != tt.want {
+				t.Errorf("isLooping() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+
+}
+
+func TestPartTwo(t *testing.T) {
+	type args struct {
+		path string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			"Part 2 Test",
+			args{
+				"./day6_input_test.txt",
+			},
+			6,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := PartTwo(tt.args.path); got != tt.want {
+				t.Errorf("PartTwo() = %v, want %v", got, tt.want)
 			}
 		})
 	}
